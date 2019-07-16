ca30df5a7f7ecbedab8e99da0aa25173f5ce240b
Mahima Gupta <mahimagupta1195@gmail.com>
2019-07-15 18:33:21 -0700

    Added complexities

--- Self-Product.java
+++ Self-Product.java
@@ -1,3 +1,10 @@
+  /*1. First Calculate the left product by maintaining the array in which ith element denotes the product of elements till i-1
+    2. Similarly calculate the right product. To save space, directly update the left product array of step1.
+    Time Complexity: O(n)
+     Space Complexity: O(1) excluding the space that output array takes
+*/
+
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int result[] = new int[nums.length];
