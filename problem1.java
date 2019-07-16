db919a8f9cb06f4db2c3cb682be7f69db388c0b9
Akhil <akhil@Akhils-MBP.hsd1.ca.comcast.net>
2019-07-16 00:54:40 -0700

    problem1 and problem3

--- problem1.java
+++ problem1.java
@@ -1,19 +1,28 @@
 public class problem1 {
 
-    public static void func(int[] arr) {
-    int prod = 1;
+    public static int[] func(int[] arr) {
+        int[] res = new int[arr.length];
+        // calculating product from left to right
+        int prod = 1;
+
         for(int i=0;i<arr.length;i++) {
-            prod = prod*arr[i];
+            res[i] = prod;
+            prod *= arr[i];
         }
-        for(int i=0;i<arr.length;i++) {
-            arr[i] = prod/arr[i];
-        }
-        for(int i=0;i<arr.length;i++) {
-            System.out.println(arr[i]);
-        }
+        //reset prod to 1;
+        prod = 1;
+        // calculating product from right to left
+        for(int i=arr.length-1;i>=0;i--) {
+            res[i] *= prod;
+            prod *= arr[i];
+        } 
+        return res;
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        func(arr);
+        
+        for(int i=0;i<func(arr).length;i++) {
+            System.out.println(func(arr)[i]);
+        }
     }
 }
