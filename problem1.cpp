3300e363a0eebb846ff06116a8c19dfa76628993
GAURAV JOSHI <gsj38@msstate.edu>
2019-07-16 00:38:59 -0500

    Updated Problem 1 Post lecture along with 2 Line Explanation and Tip

--- problem1.cpp
+++ problem1.cpp
@@ -6,9 +6,11 @@
 
 // Your code here along with comments explaining your approach
 
-/*
-1. Create a temporary pointer that checks for all the values to left of the original array.  
-2. After appending the left hand side product, iterate from end to beginning updating the values for right hand side products
+/* 2 Line Explanation
+    Create an array consisting of the product of values to the left side of the index of the array.
+    Multiply this with the product of values to the right hand side of the array
+
+    Tip: 1 Array, 2 pointers, iterate left & right of the index. Get the product. 
 */
 
 #include <iostream>
@@ -16,31 +18,33 @@
 
 using namespace std;
 
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& arr) {
-      
-        int vectorSize = arr.size();
-        vector<int> answer(vectorSize);
-        
-        int temp = 1;
-        
-        for(int i = 0; i < vectorSize; i++) 
+class Solution{
+    public:
+        vector<int>productExceptSelf(vector<int> &nums)
         {
-            answer[i] = temp; //first value to the left of index 0 is 1. Followed by product of previous values
-            temp *= arr[i];
-        }
-        
-		temp = 1; //reset the temp pointer
-     
-        for(int i = vectorSize - 1; i >= 0; i--) //Update the left filled array by traversing from the end
-        {
-            answer[i] = answer[i] * temp; //last value of answer remains the same followed by product of values to the right 
-            temp *= arr[i];
-        }
-        
-		return answer;
+            vector<int> answer(nums.size());
 
-	}
-    
+            int temp = 1; //for iterating through each elements
+            int runningProduct = 1; 
+
+            for(int i = 0; i<nums.size(); i++)
+            {  
+                answer[i] = runningProduct;
+                temp = nums[i];
+                runningProduct *= temp; 
+            }
+
+            runningProduct = 1; //reset values here
+            temp = 1;
+
+            for(int i = nums.size() - 1; i >=0; i--) 
+            {
+                answer[i] *= runningProduct;
+                temp = nums[i];
+                runningProduct *= temp;            
+            }
+
+            return answer;
+        }
+
 };
