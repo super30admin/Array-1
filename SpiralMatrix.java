53745a91956b958e43381cec0c8c9afd01dc35c3
Arivoli AE <aehari2010@gmail.com>
2019-07-15 22:52:51 -0700

    Pending Changes to problem 2

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,35 +1,73 @@
-import java.util.*;
-class SpiralMatrix {
+// import java.util.*;
+// class SpiralMatrix {
 
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if (matrix.length == 0)
-            return ans;
-        int r1 = 0, r2 = matrix.length - 1;
-        int c1 = 0, c2 = matrix[0].length - 1;
-        while (r1 <= r2 && c1 <= c2) {
-            for (int c = c1; c <= c2; c++) result.add(matrix[r1][c]);
-            for (int r = r1 + 1; r <= r2; r++) result.add(matrix[r][c2]);
-            if (r1 < r2 && c1 < c2) {
-                for (int c = c2 - 1; c > c1; c--) result.add(matrix[r2][c]);
-                for (int r = r2; r > r1; r--) result.add(matrix[r][c1]);
-            }
-            r1++;
-            r2--;
-            c1++;
-            c2--;
-        }
-        return result;
-    }
-    public static void main(String[] args){
-        System.out.println("Spiral Matrix");
-        int[][] mat = {
-            {1,2,3,11},
-            {4,5,6,12},
-            {7,8,9,13}
-        };
+//     public List<Integer> spiralOrder(int[][] matrix) {
+//         List<Integer> spiral = new ArrayList<>();
+//         if(matrix.length == 0) return spiral;
+//         int top = 0;
+//         int bottom = matrix.length-1;
+//         int left = 0;
+//         int right = matrix[0].length-1;
+        
+//         /**
+//          * L -> R TOP ++
+//          * T -> B RIGHT --
+//          * R -> L BOTTOM --
+//          * B -> T LEFT ++
+//          * 
+//          */
 
-        SpiralMatrix obj = new SpiralMatrix();
-        System.out.println(obj.spiralOrder(mat));
-    }
-}
+        
+
+//         while(top <= bottom && left <= right){
+
+//             //top row
+//             for(int i=left;i<=right;i++)
+//                 spiral.add(matrix[top][i]);
+           
+
+//             //right most row
+//             for(int i=top;i<=bottom;i++)
+//                 spiral.add(matrix[i][right]);
+            
+
+//             //bottom row
+//             if(right > left){
+//                 for(int i=right;i>=left;i--)
+//                     spiral.add(matrix[bottom][i]);
+//             }
+            
+           
+
+
+//             //left row
+//             if(bottom > top){
+//                 for(int i=bottom;i>=top;i--)
+//                     spiral.add(matrix[i][left]);
+//             }
+//             top++; 
+//             right --;
+//             bottom--;
+//             left++;
+//         }
+
+//        return spiral;
+//     }
+//     public static void main(String[] args){
+//         System.out.println("Spiral Matrix");
+//         int[][] mat = {
+//             {1,2,3,11},
+//             {4,5,6,12},
+//             {7,8,9,13}
+//         };
+
+//         // int[][] mat = {
+//         //     {1,2,3},
+//         //     {4,5,6},
+//         //     {7,8,9}
+//         // };
+
+//         SpiralMatrix obj = new SpiralMatrix();
+//         System.out.println(obj.spiralOrder(mat));
+//     }
+// }
