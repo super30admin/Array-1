dcfb6534507ec749e9c40e458449e550610e03d9
neha-0507 <53287893+neha-0507@users.noreply.github.com>
2019-09-03 15:50:14 -0700

    Update spiral_matrix.py

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -14,22 +14,22 @@ class Solution:
         colIndex = len(matrix[0])-1
         num = 0
         while a <= colIndex and b <= rowIndex:
-            ## up
+            
             
             for i in range(a,colIndex+1):
                 result.append(matrix[b][i])
             b += 1
             
-            # right
+            
             for j in range(b,rowIndex+1):
                 result.append(matrix[j][colIndex])
             colIndex -= 1
-             # down 
+             
             if b <= rowIndex:
                 for m in range(colIndex,a-1,-1): 
                     result.append(matrix[rowIndex][m])
             rowIndex -= 1 
-            # left
+            
             if a <= colIndex:
                 for n in range(rowIndex,b-1,-1):
                     result.append(matrix[n][a])
