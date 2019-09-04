5f7bd25f2f2a204fbde325c6b8b1f0597c7a07c5
Rakshitha94 <rakshithalr3007@gmail.com>
2019-09-04 14:36:12 -0700

    please review

--- productofallexcept.py
+++ productofallexcept.py
@@ -7,25 +7,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        left=[None] * len(nums)
+        left=[None] * len(nums)  #creating left array and write array of lenght equal to the  input array
         right=[None] * len(nums)
-                # left=[]
+                # left=[]   #not sure why this doesnt work
                 # right=[]
-        left[0]=1
-        right[(len(nums)-1)]=1
-        for i in range(1,len(nums)):
-            left[i]=left[i-1]*nums[i-1]
+        left[0]=1 #making the value of the first element of array as 1, beacuase there is nothing on the left side of the first element in the
+                                    #in the input array . We will traversing from left to right
+        right[(len(nums)-1)]=1   #since we are traversing from right to left, there is nothing to the rightside of the last element in the input arrau
+                                        #hence making it 1
+        for i in range(1,len(nums)):    #travsering through the nums array from 1. (left to righ)
+            left[i]=left[i-1]*nums[i-1]    ##getting the product of all the elements before the ith element
 
-        for j in range((len(nums)-2),-1,-1):
-            right[j]=right[j+1]*nums[j+1]
+        for j in range((len(nums)-2),-1,-1):   #traversing from right to left
+            right[j]=right[j+1]*nums[j+1]           #getting the product of all elements before ith element
 
         for i in range(len(nums)):
-            left[i]=left[i]*right[i]
+            left[i]=left[i]*right[i]   #final - to get output, mulitply ith element of left array to the ith index of right array
 
         return left
 
 
-##### using variable ####3
+##### using variable ####
 
 class Solution(object):
     def productExceptSelf(self, nums):
@@ -34,21 +36,23 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        left = [None] * len(nums)
+        left = [None] * len(nums)    #here instead of creating two separate right and left arrays, we will create just one extra array left.
+                                            #here we are not creating right array. Saving space complexity
         output = [None] * len(nums)
         # left=[]
         # right=[]
         left[0] = 1
         # right[(len(nums)-1)]=1
-        for i in range(1, len(nums)):
+        for i in range(1, len(nums)):      #doing the same steps like above just for the above
             left[i] = left[i - 1] * nums[i - 1]
         # print(left)
 
-        right = 1
+        right = 1                               #we are creating a variable r.
         for i in range((len(nums) - 1), -1, -1):
-            left[i] = left[i] * right
+            left[i] = left[i] * right               #this variable will multiply the left index
             # print(left)
-            right = right * nums[i]
+            right = right * nums[i]             #this r will get the value of nums from right to left
+                                                #which will be multiplies with left elements
 
         return left
 
--- spiralmatrix.py
+++ spiralmatrix.py
@@ -1,6 +1,9 @@
 #time complexity: 0(n)
 
-
+#here we either move up or move down.
+#while moving up, we decrease the row and increase the coloumn and
+#while moving down, we increase the row and decrease the coloumn'
+#while moving up and down, we check for boundary conditions
 
 class Solution(object):
     def findDiagonalOrder(self, matrix):
@@ -10,28 +13,33 @@ class Solution(object):
         """
         if not matrix:
             return []
-        direction = 1
+        direction = 1  #indication to move up
         result = []
         nrows = len(matrix) - 1
         ncols = len(matrix[0]) - 1
         row = 0
         cols = 0
-        while (len(result) < (nrows + 1) * (ncols + 1)):
+        while (len(result) < (nrows + 1) * (ncols + 1)):  #until we have all the elements of the matrix in the result array
             result.append(matrix[row][cols])
-            if direction == 1:
-                if cols == ncols:
+            if direction == 1:    #while moving up
+                if cols == ncols:    #if the cols reach maximum, we have to increase row and move down
+
+                                    #and always this condition should be first because, when we put the below condition down,
+                                    #and when the cols reach last col, the row will be still zero and if we add coloumn , we get out of index
                     row = row + 1
                     direction = 0
-                elif row == 0:
+                elif row == 0:          #before the rows become -1, that is when row is 0, move down by increasing the coloumn
                     cols = cols + 1
                     direction = 0
                 else:
-                    row = row - 1       #
+                    row = row - 1       #we simply move up
                     cols = cols + 1
 
             else:
                 if row == nrows:
-                    cols = cols + 1
+                    cols = cols + 1             #while moving down, if the rowa=s reach last row, we have to increase the oloumn and start going up.
+                                            #we have to write this condition first beacuse, wheh we are in the first col and last eleemtn and we increase
+                                                #row like the below condition , we will encounter out of index
                     direction = 1
 
                 elif cols == 0:
@@ -39,7 +47,7 @@ class Solution(object):
                     direction = 1
 
                 else:
-                    row = row + 1
+                    row = row + 1               #we simply move down
                     cols = cols - 1
                     # row=row+1
         return result
