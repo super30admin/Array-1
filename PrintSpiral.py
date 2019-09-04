d1b1bda3c6891d757a1313e44e8499cbcbfd48c4
Vishwanath Patil <vishwanath.patil@sjsu.edu>
2019-09-03 23:37:26 -0700

    Completed

--- PrintSpiral.py
+++ PrintSpiral.py
@@ -1,36 +1,39 @@
 """
-The approach with the comments. Solution from GeeksforGeeks
-"""
-def spiralPrint(m, n, a) : 
-    k = 0; l = 0
-  
-    while (k < m and l < n) : 
-          
-        # For the first row
-        for i in range(l, n) : 
-            print(a[k][i], end = " ") 
-              
-        k += 1
-  
-        # For the last column
-        for i in range(k, m) : 
-            print(a[i][n - 1], end = " ") 
-              
-        n -= 1
-  
-        # For the last row 
-        if ( k < m) : 
-              
-            for i in range(n - 1, (l - 1), -1) : 
-                print(a[m - 1][i], end = " ") 
-              
-            m -= 1
-          
-        # For the first column from remaning
-        if (l < n) : 
-            for i in range(m - 1, k - 1, -1) : 
-                print(a[i][l], end = " ") 
-              
-            l += 1
+The approach is to keep on moving the spiral order. first print the first row. then the last column
+then the last row and then the first column. meanwhile also keep bottom and top pointers and left 
+and right pointers. increment and decrement those accoridingly to the required path of printing.
 
-        # Continue this until you terminate the while condition.
+Leetcode running.
+Time complexity and space compexity - O(N), O(1)
+"""
+def spiralOrder(self, matrix):
+        if not matrix:
+            return []
+        top = 0
+        bottom = len(matrix) - 1
+        lc = 0
+        rc = len(matrix[0]) - 1
+        res = []
+        
+        
+
+        while top<= bottom and lc <= rc:
+            
+            for i in range(lc,rc+1):
+                res.append(matrix[top][i])
+            top += 1
+            
+            for i in range(top,bottom+1):
+                res.append(matrix[i][rc])
+            rc -= 1
+            
+            if top <= bottom:
+                for i in range(rc,lc-1,-1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+                
+            if lc <= rc:
+                for i in range(bottom,top-1,-1):
+                    res.append(matrix[i][lc])
+                lc += 1
+        return res
