e873fcd12ff7cd3d501d700673ffa03ba26d68ac
pradhanyash <yash_pradhan@hotmail.com>
2019-09-03 21:05:41 -0500

    refactored logic, much more intuitive now

--- SpiralOrder.java
+++ SpiralOrder.java
@@ -2,15 +2,15 @@
  * Given a matrix of m x n elements (m rows, n columns), return all elements of
  * the matrix in spiral order.
  * 
- * Space Complexity: O(MN)
- * Time Complexity: O(MN)
+ * Space Complexity: O(MN) Time Complexity: O(MN)
  * 
- * As discussed, this problem tests the critical thinking ability on bookkeeping that needs to be done
- * Maintaining indices here and handling cases where we hit the edges is important!
+ * As discussed, this problem tests the critical thinking ability on bookkeeping
+ * that needs to be done Maintaining indices here and handling cases where we
+ * hit the edges is important!
  * 
- * Leetcode Result:
- * Runtime: 0 ms, faster than 100.00% of Java online submissions for Spiral Matrix.
- * Memory Usage: 34.6 MB, less than 100.00% of Java online submissions for Spiral Matrix.
+ * Leetcode Result: Runtime: 0 ms, faster than 100.00% of Java online
+ * submissions for Spiral Matrix. Memory Usage: 34.6 MB, less than 100.00% of
+ * Java online submissions for Spiral Matrix.
  */
 public class SpiralOrder {
     /**
@@ -25,7 +25,7 @@ public class SpiralOrder {
             return result;
         }
 
-        // these 2 variables help maintain bounds
+        // these variables help maintain bounds
         int top = 0, bottom = matrix.length - 1;
         int left = 0, right = matrix[0].length - 1;
         int cursor = 0; // current element being processed
@@ -35,24 +35,26 @@ public class SpiralOrder {
             for (cursor = left; cursor <= right; cursor++) {
                 result.add(matrix[top][cursor]);
             }
+            top++;
 
-            for (cursor = top + 1; cursor <= bottom; cursor++) {
+            for (cursor = top; cursor <= bottom; cursor++) {
                 result.add(matrix[cursor][right]);
             }
+            right--;
 
-            if (top < bottom && left < right) { // make sure that we are within bounds
-                for (cursor = right - 1; cursor > left; cursor--) {
+            if (top <= bottom) { // make sure that we are within bounds
+                for (cursor = right; cursor >= left; cursor--) {
                     result.add(matrix[bottom][cursor]);
                 }
-                for (cursor = bottom; cursor > top; cursor--) {
+            }
+            bottom--;
+
+            if (left <= right) { // make sure that we are within bounds
+                for (cursor = bottom; cursor >= top; cursor--) {
                     result.add(matrix[cursor][left]);
                 }
             }
-
-            top++;
-            bottom--;
             left++;
-            right--;
         }
         return result;
     }
