993839f5f07e345fc0a449f592ac659505e40bf9
Rakshitha94 <rakshithalr3007@gmail.com>
2019-09-05 02:34:06 -0700

    please review

--- spiralmatrix.py
+++ spiralmatrix.py
@@ -1,53 +1,46 @@
-#time complexity: 0(n)
+#spiral matrix
+#time complexity : 0(n)
+# felt little difficult to track the row and columns
+#no doubts
+#ran on leetcode
 
-#here we either move up or move down.
-#while moving up, we decrease the row and increase the coloumn and
-#while moving down, we increase the row and decrease the coloumn'
-#while moving up and down, we check for boundary conditions
+#we have four different pointer, right coloumn(RC),left coloumn(LC), top row(TR), bottom row(BR)
+# while going left, we traverse the top row (TR) from LC to RC, after printing all elements from top row, we increment the row which will go to next row
+#while going down, we move RC from TR to BR, and do RC=Rc-1
+#while going left, traverse BR from RC to LC (backward), then do BR=Br-1. cos we printed all the leemts of the last row
+#while going up, we traverse LC from BR to TR and then do LC=LC+1
+
+#after incrementing the TR,Br,LC,RC we should always check if TR<=Br and Lc<=Rc. because when it crossed it means we have printed all the eleemnts
+
+
 
 class Solution(object):
-    def findDiagonalOrder(self, matrix):
+    def spiralOrder(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        if not matrix:
-            return []
-        direction = 1  #indication to move up
         result = []
-        nrows = len(matrix) - 1
-        ncols = len(matrix[0]) - 1
-        row = 0
-        cols = 0
-        while (len(result) < (nrows + 1) * (ncols + 1)):  #until we have all the elements of the matrix in the result array
-            result.append(matrix[row][cols])
-            if direction == 1:    #while moving up
-                if cols == ncols:    #if the cols reach maximum, we have to increase row and move down
+        if len(matrix) == 0:
+            return result
+        tr = 0
+        rc = len(matrix[0]) - 1  # 2
+        lc = 0
+        br = len(matrix) - 1  # 3
 
-                                    #and always this condition should be first because, when we put the below condition down,
-                                    #and when the cols reach last col, the row will be still zero and if we add coloumn , we get out of index
-                    row = row + 1
-                    direction = 0
-                elif row == 0:          #before the rows become -1, that is when row is 0, move down by increasing the coloumn
-                    cols = cols + 1
-                    direction = 0
-                else:
-                    row = row - 1       #we simply move up
-                    cols = cols + 1
-
-            else:
-                if row == nrows:
-                    cols = cols + 1             #while moving down, if the rowa=s reach last row, we have to increase the oloumn and start going up.
-                                            #we have to write this condition first beacuse, wheh we are in the first col and last eleemtn and we increase
-                                                #row like the below condition , we will encounter out of index
-                    direction = 1
-
-                elif cols == 0:
-                    row = row + 1
-                    direction = 1
-
-                else:
-                    row = row + 1               #we simply move down
-                    cols = cols - 1
-                    # row=row+1
+        while ((tr <= br) and (lc <= rc)): #checking pointers
+            for i in range(lc, rc + 1):  #traversing left to right
+                result.append(matrix[tr][i])
+            tr = tr + 1
+            for j in range(tr, br + 1): #traversing from up to down
+                result.append(matrix[j][rc])
+            rc = rc - 1
+            if ((tr <= br)):
+                for i in range(rc, lc - 1, -1): #traversing from right to left
+                    result.append(matrix[br][i])
+                br = br - 1
+            if (lc <= rc):
+                for i in range(br, tr - 1, -1): #traversing from down to up
+                    result.append(matrix[i][lc])
+                lc = lc + 1
         return result
