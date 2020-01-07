c161e5af8309ce307489f2f77d1be240ba8d33cd
Siddharth Mehta <mehtasiddharth1991@gmail.com>
2020-01-07 18:59:34 +0530

    Product Except Self Optimization

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -10,23 +10,17 @@ class ProductExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         if(nums == null || nums.length == 0) return new int[]{0};
         int length = nums.length;
-        
-        int[] L = new int[length];
-        int[] R = new int[length];
         int[] answer = new int[length];
         
-        L[0] = 1;
+        answer[0] = 1;
         for(int i = 1; i < length; i++){
-            L[i] = nums[i - 1] * L[i - 1];
+            answer[i] = nums[i - 1] * answer[i - 1];
         }
         
-        R[length - 1] = 1;
-        for(int i = length - 2; i >= 0; i--){
-            R[i] = nums[i + 1] * R[i + 1];
-        }
-        
-        for(int i = 0; i < length; i++){
-            answer[i] = L[i] * R[i];
+        int rp = 1;
+        for(int i = length - 1; i >= 0; i--){
+            answer[i] = answer[i] * rp;
+            rp *= nums[i];
         }
         
         return answer;
