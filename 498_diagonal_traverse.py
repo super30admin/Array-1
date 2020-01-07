f5f0ccacb717db0f97dff470fe39410c7b680d9a
Nikhil Agrawal <nikrasiya@gmail.com>
2020-01-07 07:48:43 -0500

    all problems complete as per class

--- 498_diagonal_traverse.py
+++ 498_diagonal_traverse.py
@@ -7,6 +7,62 @@ class Solution:
     def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
         """
             // Time Complexity : O(mn)
+            // Space Complexity : O(1)
+            // Did this code successfully run on Leetcode :
+                    Yes
+            // Three line explanation of solution in plain english :
+                    - We have two directions to traverse, up and down
+                    - For each direction we have three case
+                        - We are at the end of the col (up direction)
+                          or we are at the end of the row (down direction)
+                        - We are at the start of the row (up direction)
+                          or we are the start of column (down direction)
+                        - We are some where in between (normal case)
+                    - For each such case we adjust the row and col values
+                      and direction
+        """
+        # direction we are traversing
+        # 1 means up, -1 means down
+        rows = len(matrix)
+        cols = len(matrix[0])
+        dir = 1
+        row = col = 0
+        result = []
+        while len(result) < rows * cols:
+            result.append(matrix[row][col])
+            # Up Direction
+            if dir == 1:
+                # case 1
+                if col == cols - 1:
+                    row += 1
+                    dir = -1
+                # case 2
+                elif row == 0:
+                    col += 1
+                    dir = -1
+                # case 3
+                else:
+                    row -= 1
+                    col += 1
+            # Down direction
+            elif dir == -1:
+                # case 1
+                if row == rows - 1:
+                    col += 1
+                    dir = 1
+                # case 2
+                elif col == 0:
+                    row += 1
+                    dir = 1
+                # case 3
+                else:
+                    row += 1
+                    col -= 1
+        return result
+
+    def find_diagonal_order_using_deque(self, matrix: List[List[int]]) -> List[int]:
+        """
+            // Time Complexity : O(mn)
             // Space Complexity : O(mn)
             // Did this code successfully run on Leetcode :
                     Yes
@@ -45,14 +101,14 @@ if __name__ == '__main__':
         [4, 5, 6],
         [7, 8, 9]
     ]))
-    # h.findDiagonalOrder([
-    #     [1, 2, 3, 4, 5],
-    #     [6, 7, 8, 9, 10],
-    #     [11, 12, 13, 14, 15],
-    #     [16, 17, 18, 19, 20]
-    # ])
-    # print(h.findDiagonalOrder([
-    #     [1, 2],
-    #     [4, 5],
-    #     [7, 8]
-    # ]))
+    print(h.findDiagonalOrder([
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20]
+    ]))
+    print(h.findDiagonalOrder([
+        [1, 2],
+        [4, 5],
+        [7, 8]
+    ]))
--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -33,20 +33,63 @@ class Solution:
         cols = len(matrix[0])
         top, bottom, left, right = 0, rows - 1, 0, cols - 1
 
+        # in each while loop we do one loop (right, down, left, up)
         while len(result) < rows * cols:
             for col in range(left, right + 1):
-                if len(result) < rows * cols: result.append(matrix[top][col])
+                result.append(matrix[top][col])
             top += 1
             for row in range(top, bottom + 1):
-                if len(result) < rows * cols: result.append(matrix[row][right])
+                result.append(matrix[row][right])
             right -= 1
-            for col in reversed(range(left, right + 1)):
-                if len(result) < rows * cols: result.append(matrix[bottom][col])
-            bottom -= 1
-            for row in reversed(range(top, bottom + 1)):
-                if len(result) < rows * cols: result.append(matrix[row][left])
-            left += 1
+            if top <= bottom:
+                for col in reversed(range(left, right + 1)):
+                    result.append(matrix[bottom][col])
+                bottom -= 1
+            if left <= right:
+                for row in reversed(range(top, bottom + 1)):
+                    result.append(matrix[row][left])
+                left += 1
         return result
+
+    def spiral_order_recursive(self, matrix: List[List[int]]) -> List[int]:
+        """
+            // Time Complexity : O(mn)
+                    'm' is the row, 'n' is the column
+            // Space Complexity : O(min(m,n))
+            // Did this code successfully run on Leetcode : Yes
+            // Three line explanation of solution in plain english
+                    - The logic is similar to the iterative solution
+                    - The keen observation is after one loop (right, down, left, up)
+                      the next traversal is a recursive structure.
+        """
+        # edge case
+        result = []
+        if not matrix:
+            return result
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self._helper(matrix, 0, rows - 1, 0, cols - 1, result)
+        return result
+
+    def _helper(self, matrix: List[List[int]], top, bottom, left, right, result: List[int]) -> None:
+        # base case
+        if left > right or top > bottom: return
+        # logic
+        for col in range(left, right + 1):
+            result.append(matrix[top][col])
+        top += 1
+        for row in range(top, bottom + 1):
+            result.append(matrix[row][right])
+        right -= 1
+        if top <= bottom:
+            for col in reversed(range(left, right + 1)):
+                result.append(matrix[bottom][col])
+            bottom -= 1
+        if left <= right:
+            for row in reversed(range(top, bottom + 1)):
+                result.append(matrix[row][left])
+            left += 1
+        self._helper(matrix, top, bottom, left, right, result)
 
 
 if __name__ == '__main__':
@@ -57,6 +100,16 @@ if __name__ == '__main__':
         [7, 8, 9]
     ]))
     print(h.spiralOrder([
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ]))
+    print(h.spiral_order_recursive([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]))
+    print(h.spiral_order_recursive([
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 10, 11, 12]
