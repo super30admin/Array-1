091a17f4ae5521b46a3cd78a1e03dc33c1b34d21
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-17 13:49:35 -0800

    Array-1 done

--- ProductExceptItself_238.java
+++ ProductExceptItself_238.java
@@ -1,1 +1,56 @@
-ProductExceptItself_238.java
+// Time Complexity : O(N) for both
+// Space Complexity : O(N) for approach-1 and O(1) for approach-2
+// Did this code successfully run on Leetcode : yes
+
+
+/*
+Approach 
+========
+1. First update the result array with product of every element's left elements.
+2. Now, iterate from right to left, keeping track of right product so far using a "prod" variable. 
+3. Multiply value in result array with the prod variable and keep updating prod variable to get product of array elements except itself.
+*/
+
+// ==================== Approach - 1 using O(N) space ===============================
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        int prod = 1;
+        left[0] = 1;
+        for( int i = 1; i < nums.length; i++) {
+            left[i] = nums[i-1]*left[i-1];
+        }
+        
+        right[nums.length - 1] = 1;
+        prod = 1;
+        for( int i = nums.length-2; i >= 0; i--) {
+            right[i] = nums[i+1]*right[i+1];
+        }
+        for(int i = 0; i < nums.length; i++) {
+            right[i] = right[i] * left[i];
+        }
+        
+        return right;
+    }
+}
+
+// ==================== Approach - 2 using constant space ===============================
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for(int i = 1; i < nums.length; i++) {
+            result[i] = result[i-1]*nums[i-1];
+        }
+        int right = 1;
+        for(int i = nums.length-1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+        return result;
+    }
+}
+
