1ddaeb370167f859c7fc200a01dcd632b8191703
SomiDeev <sowmyadvn.92@gmail.com>
2020-02-17 13:52:26 -0800

    update approach

--- ProductExceptItself_238.java
+++ ProductExceptItself_238.java
@@ -19,15 +19,20 @@ class Solution {
         int[] right = new int[nums.length];
         int prod = 1;
         left[0] = 1;
+        // left product
         for( int i = 1; i < nums.length; i++) {
             left[i] = nums[i-1]*left[i-1];
         }
         
         right[nums.length - 1] = 1;
         prod = 1;
+
+        //right product
         for( int i = nums.length-2; i >= 0; i--) {
             right[i] = nums[i+1]*right[i+1];
         }
+
+        //updating array with the result
         for(int i = 0; i < nums.length; i++) {
             right[i] = right[i] * left[i];
         }
@@ -42,14 +47,20 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
         result[0] = 1;
+
+        // calculate left sub-product
         for(int i = 1; i < nums.length; i++) {
             result[i] = result[i-1]*nums[i-1];
         }
+
+        // Use right variable to keep track of right product so far and update result array with right subproduct as well
+        // result would be result of right and left subproducts after this step
         int right = 1;
         for(int i = nums.length-1; i >= 0; i--) {
             result[i] *= right;
             right *= nums[i];
         }
+
         return result;
     }
 }
