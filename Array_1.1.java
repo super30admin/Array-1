3ecf40d708d7b14f1075838d2e1efe6963198adb
Sai Swaroop Reddy Thammineni <54874974+saiswaroopreddy@users.noreply.github.com>
2020-02-18 17:22:12 -0800

    Updated with optimal complexity

--- Array_1.1.java
+++ Array_1.1.java
@@ -5,15 +5,24 @@
 //Find the product of all elements by traversing once through the list and save in a variable.
 //Now mutate the same list-  dividing the product by the current element and storing. 
 
-class Solution{
-    public int[] productExceptSelf(int [] nums){
-        int var = 1;
-        for(int i =0;i<nums.length;i++){
-            var = var * nums[i];
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+       if(nums.length == 0 || nums == null ) return new int[] {0};
+        int n = nums.length;
+        
+        int left =1, right =1;
+        int[] ans = new int[nums.length];
+        ans[0]=1;
+        
+        for(int i =1;i<n;i++){
+            ans[i] = left * nums[i-1];
+            left = ans[i];
         }
-        for(int i =0;i<nums.length;i++){
-            nums[i] = var/nums[i];
+        for(int i = n-1; i>=0;i--){
+            ans[i] = right * ans[i];
+            right *= nums[i];
         }
-        return nums;
+        return ans;
+        
     }
 }
