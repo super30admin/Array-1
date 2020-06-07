7b6d6cbe0dd1f86384fefd938defea02899c0596
chirag <cr.tagadiya@gmail.com>
2020-06-07 17:55:22 -0400

    Constant space tried, linear space working

--- Product_Of_Array_Except_Self.java
+++ Product_Of_Array_Except_Self.java
@@ -6,6 +6,31 @@
 */
 
 public class Product_Of_Array_Except_Self{
+    private static double log2(int N) 
+    { 
+        double result = (double)(Math.log(N) / Math.log(2)); 
+        return result; 
+    } 
+    public int[] productExceptSelf_Constant_Space(int[] nums) {
+        
+        if (nums == null || nums.length == 0 ) return new int[nums.length];
+        int n = nums.length;
+        
+        int product = 1;
+        for(int num : nums){
+            product *= num;
+        }
+        double log_product = log2(product);
+       
+        
+        for(int i=0; i < nums.length; i++){
+            double anti = log_product - log2(nums[i]);
+            nums[i] = (int) Math.pow(2,anti);
+            // System.out.print("AntiLog "+anti);
+        }
+        return nums;
+    }
+
     public int[] productExceptSelf(int[] nums) {
         
         if (nums == null || nums.length == 0 ) return new int[nums.length];
