ed424f18c4c9ca54feb5dbba494bd0fbe7eae808
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-06-08 16:32:57 -0700

    Create Problem_2.java

---
+++ Problem_2.java
@@ -0,0 +1,43 @@
+// Time complexity - O(mn)
+// Space Complexity - O(1)
+
+// Iterate from index [0,0] and change direction and index based on row and column value.
+
+class Solution {
+  public int[] findDiagonalOrder(int[][] matrix) {
+      if(matrix == null || matrix.length == 0) return new int[0];
+      int m = matrix.length;
+      int n = matrix[0].length;
+      int [] result = new int[m*n];
+      int r = 0, c = 0;
+      int i = 0;
+      int dir = 1;
+      while(i < m*n){
+          result[i] = matrix[r][c];
+          if(dir == 1){
+              if(c==n-1) {
+                  r++;
+                  dir = -1;
+              } else if (r==0) {
+                  c++;
+                  dir = -1;
+              } else{
+                  r--;
+                  c++;
+              }
+          } else {
+              if(r == m-1) {
+                  c++;
+                  dir = 1;
+              } else if(c==0) {
+                  r++;
+                  dir = 1;
+              } else {
+                  r++; c--;
+              }
+          }
+          i++;
+      }
+      return result;
+  }
+}
