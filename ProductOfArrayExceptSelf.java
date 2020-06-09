0443f297f67c641141451727168678b1caf4bbb0
GreeshmaReddy <greeshma.naredla@gmail.com>
2020-06-09 11:08:14 +0530

    Array1 Problem1

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -1,12 +1,13 @@
 //Bruteforce
 
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        // Time Complexity : O( nums * nums)
+
+            // Time Complexity : O( nums * nums)
 // Space Complexity : O(nums)
 // Did this code successfully run on Leetcode : Time Limit Exceedng
 
-        
+    public int[] productExceptSelf(int[] nums) {
+     
         if(nums == null || nums.length == 0) return new int[nums.length];
         
         int[] output = new int[nums.length];
@@ -23,3 +24,42 @@ class Solution {
         
     }
 }
+
+
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+// Time Complexity : O( nums )
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Your code here along with comments explaining your approach:
+
+/*
+Idea is that value at each index will be (product of all nums on left) * (product of all nums on right)
+1.Use a result array, then first maintain the left product of all nums 
+2. Then in second pass while calc the right product of all nums use the result array directly and multiply the right product to result.
+*/
+        
+        
+        
+        if(nums == null || nums.length == 0) return new int[0];
+        
+        int[] result = new int[nums.length];
+        
+        int rp = 1; int temp = 1;
+        for(int i =0;i<nums.length;i++){
+            rp = rp*temp;
+            result[i] = rp;
+            temp = nums[i];
+        }     
+        rp = 1; temp = 1;
+        for(int i =nums.length-1;i>=0;i--){
+            rp = rp* temp;
+            result[i] = result[i] * rp;
+            temp = nums[i];
+        }
+        return result;
+        
+    }
+}
+
