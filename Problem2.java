a7c31e74c858afb4e90655d5630f19898b19164b
soumil.sarvaiya <ss8165@rit.edu>
2020-07-21 15:09:47 -0400

    Completed Problem2

--- Problem2.java
+++ Problem2.java
@@ -1,7 +1,79 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n*m)
+//      n: number of rows
+//      m: number of columns
+// Space Complexity : O(1)
+//      Used the input matrix
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+class Problem2 {
+
+    /** find diagonal order */
+    public int[] findDiagonalOrder(int[][] matrix) {
+
+        // base condition
+        if(matrix==null || matrix.length==0)
+            return new int[]{};
+        
+        // initialize
+        int n= matrix.length;
+        int m= matrix[0].length;
+        int row = 0;
+        int col = 0;
+        boolean directionUp = true;
+        int count = 0;
+        int maxCount = n * m;
+        
+        int[] result = new int[maxCount];
+        
+        // till all the elements are visited
+        while(count < maxCount){
+            
+            // adding to result
+            result[count] = matrix[row][col];
+            count++;
+
+            // for direction Up
+            if(directionUp){
+                
+                // if reached column Upper bound
+                if(col== m-1){
+                    row ++;
+                    directionUp = false; // changing direction
+                } // if reached row lower bound
+                else if(row==0){
+                    col ++;
+                    directionUp = false; // changing direction
+                // going up ~ decreasing row & increasing col
+                }else{
+                    row--;
+                    col++;
+                }
+            // for direction down  
+            }else{
+                
+                // reached row upper bound
+                if(row==n-1){
+                    col++;
+                    directionUp = true; // changing direction
+                } // reached col lower bound
+                else if(col==0){
+                    row++;
+                    directionUp = true; // changing direction
+                // going down ~ increasing row & decreasing col
+                }else{
+                    row++;
+                    col--;
+                }  
+            }
+            
+        }
+
+        // return result
+        return result;
+    }
+}
+
