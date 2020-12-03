43d6038fd0a3a58ee1440f32e9ea34a356d01447
sourav singh <souravsingh@souravs-MacBook-Air.local>
2020-12-03 21:53:24 +0530

    Array solution

--- Sample.java
+++ Sample.java
@@ -1,6 +1,159 @@
-// Time Complexity :
+// Time Complexity :O(N)
 // Space Complexity :
 // Did this code successfully run on Leetcode :
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+
+//Product of Array Except Self
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        // The length of the input array
+        int length = nums.length;
+
+        // The left and right arrays as described in the algorithm
+        int[] L = new int[length];
+        int[] R = new int[length];
+
+        // Final answer array to be returned
+        int[] answer = new int[length];
+
+        // L[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the left,
+        // so L[0] would be 1
+        L[0] = 1;
+        for (int i = 1; i < length; i++) {
+
+            // L[i - 1] already contains the product of elements to the left of 'i - 1'
+            // Simply multiplying it with nums[i - 1] would give the product of all
+            // elements to the left of index 'i'
+            L[i] = nums[i - 1] * L[i - 1];
+        }
+
+        // R[i] contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to the right,
+        // so the R[length - 1] would be 1
+        R[length - 1] = 1;
+        for (int i = length - 2; i >= 0; i--) {
+
+            // R[i + 1] already contains the product of elements to the right of 'i + 1'
+            // Simply multiplying it with nums[i + 1] would give the product of all
+            // elements to the right of index 'i'
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        // Constructing the answer array
+        for (int i = 0; i < length; i++) {
+            // For the first element, R[i] would be product except self
+            // For the last element of the array, product except self would be L[i]
+            // Else, multiple product of all elements to the left and to the right
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+}
+//Diagonal Traverse
+
+class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        
+        // Check for empty matrices
+        if (matrix == null || matrix.length == 0) {
+            return new int[0];
+        }
+        
+        // Variables to track the size of the matrix
+        int N = matrix.length;
+        int M = matrix[0].length;
+        
+        // Incides that will help us progress through 
+        // the matrix, one element at a time.
+        int row = 0, column = 0;
+        
+        // As explained in the article, this is the variable
+        // that helps us keep track of what direction we are
+        // processing the current diaonal
+        int direction = 1;
+        
+         // The final result array
+        int[] result = new int[N*M];
+        int r = 0;
+        
+        // The uber while loop which will help us iterate over all
+        // the elements in the array.
+        while (row < N && column < M) {
+            
+            // First and foremost, add the current element to 
+            // the result matrix. 
+            result[r++] = matrix[row][column];
+            
+            // Move along in the current diagonal depending upon
+            // the current direction.[i, j] -> [i - 1, j + 1] if 
+            // going up and [i, j] -> [i + 1][j - 1] if going down.
+            int new_row = row + (direction == 1 ? -1 : 1);
+            int new_column = column + (direction == 1 ? 1 : -1);
+            
+            // Checking if the next element in the diagonal is within the
+            // bounds of the matrix or not. If it's not within the bounds,
+            // we have to find the next head. 
+            if (new_row < 0 || new_row == N || new_column < 0 || new_column == M) {
+                
+                // If the current diagonal was going in the upwards
+                // direction.
+                if (direction == 1) {
+                    
+                    // For an upwards going diagonal having [i, j] as its tail
+                    // If [i, j + 1] is within bounds, then it becomes
+                    // the next head. Otherwise, the element directly below
+                    // i.e. the element [i + 1, j] becomes the next head
+                    row += (column == M - 1 ? 1 : 0) ;
+                    column += (column < M - 1 ? 1 : 0);
+                        
+                } else {
+                    
+                    // For a downwards going diagonal having [i, j] as its tail
+                    // if [i + 1, j] is within bounds, then it becomes
+                    // the next head. Otherwise, the element directly below
+                    // i.e. the element [i, j + 1] becomes the next head
+                    column += (row == N - 1 ? 1 : 0);
+                    row += (row < N - 1 ? 1 : 0);
+                }
+                    
+                // Flip the direction
+                direction = 1 - direction;        
+                        
+            } else {
+                
+                row = new_row;
+                column = new_column;
+            }
+        }
+        return result;      
+    }
+}
+
+//Spiral Matrix
+class Solution {
+    public List < Integer > spiralOrder(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
