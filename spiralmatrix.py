9843d8720c5bd6eb755ddda1cd870f56049208fd
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-01-03 23:25:55 -0700

    Create spiralmatrix.py

---
+++ spiralmatrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        x = []
+        k=0
+        while(len(x)<len(matrix)*len(matrix[0])):
+            i = k
+            j = k
+            while (j<len(matrix[0])-k) and len(x)<len(matrix)*len(matrix[0]):
+                x.append(matrix[i][j])
+                j+=1
+            
+            j-=1
+            i+=1
+            while (i<len(matrix)-k) and (len(x)<len(matrix)*len(matrix[0])):
+                x.append(matrix[i][j])
+                i+=1
+            i-=1
+            j-=1
+            
+            while(j>=k) and (len(x)<len(matrix)*len(matrix[0])):
+                x.append(matrix[i][j])
+                j-=1
+            j+=1
+            i-=1
+            while(i>k) and (len(x)<len(matrix)*len(matrix[0])):
+                x.append(matrix[i][j])
+                i-=1
+            
+            k+=1
+            
+        return x
