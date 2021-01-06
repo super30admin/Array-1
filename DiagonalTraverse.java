736740bc884df10b91d932366c5324842c809894
Yasunori Iwayanagi <yiwayanagi@upward.net>
2021-01-06 12:32:56 -0800

    Self Submission Diagonal Traverse

--- DiagonalTraverse.java
+++ DiagonalTraverse.java
@@ -1,47 +1,45 @@
 // Time Complexity : O(R*C)
 // Space Complexity : O(R*C)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Figuring out pattern where all values in same diagonal share same sum value of x idnex and y index. 
+// Any problem you faced while coding this : Using the direction variable, and figuring out the possible edge cases.
 
 // Your code here along with comments explaining your approach
 
 
 class Solution {
-    public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0){
-            return new int[0];
-        } 
-
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int[] res = new int[rows * cols];
-        int r = 0;
-        int c = 0;
-        for (int i = 0; i < rows * cols; i++) {
-          res[i] = matrix[r][c];
-          // even sum diagonal
-          if ((r + c) % 2 == 0) {
-            if (r - 1 >= 0 && c + 1 < cols) {
-              c = c + 1;
-              r = r - 1;
-            } else if (r - 1 < 0 && c + 1 < cols) {
-              c = c + 1;
-            } else if (r + 1 < rows && c + 1 > cols - 1) {
-              r = r + 1;
-            }
+  public int[] findDiagonalOrder(int[][] matrix) {
+      if(matrix == null || matrix.length == 0) {
+          return new int[0];
+      }
+      int[] output = new int[matrix.length*matrix[0].length];
+      int row = 0, col = 0, dir = 1;
+      int resultIndex = 0;
+      while(row < matrix.length && col < matrix[0].length){
+          output[resultIndex++] = matrix[row][col];
+          if(dir == 1){
+              if(col == matrix[0].length-1){
+                  row++;
+                  dir = -1;
+              }else if(row == 0){
+                  dir = -1;
+                  col++;
+              }else{
+                  row--;
+                  col++;
+              }
+          }else if(dir == -1){
+              if(row == matrix.length-1){
+                  col++;
+                  dir = 1;
+              }else if(col == 0){
+                  dir = 1;
+                  row++;
+              }else{
+                  row++;
+                  col--;
+              }
           }
-          // odd sum diagonal
-          else if ((r + c) % 2 != 0) {
-            if (r + 1 < rows && c - 1 >= 0) {
-              c = c - 1;
-              r = r + 1;
-            } else if (r + 1 < rows && c - 1 < 0) {
-              r = r + 1;
-            } else if (r + 1 > rows - 1 && c +1 < cols) {
-              c = c + 1;
-            }
-          }
-        }
-        return res;
-    }
+      }
+      return output;
+  }
 }
