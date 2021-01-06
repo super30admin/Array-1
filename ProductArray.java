2631ecf08b8b3c0bedb78901cd31f987ba710678
Yasunori Iwayanagi <yiwayanagi@upward.net>
2021-01-06 11:36:40 -0800

    Product Array Self Revised

--- ProductArray.java
+++ ProductArray.java
@@ -1,26 +1,41 @@
 // Time Complexity : O(N)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Figuring out solutions using no division. 
+// Any problem you faced while coding this : Knowing to use left and right product lists. 
 // Your code here along with comments explaining your approach
 
-public class Solution {
+class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int leng = nums.length;
-        int[] ret = new int[leng];
-        if(leng == 0)
-            return ret;
-        int runningprefix = 1;
-        for(int i = 0; i < leng; i++){
-            ret[i] = runningprefix;
-            runningprefix*= nums[i];
+        if(nums == null || nums.length == 0) {
+            return nums;
         }
-        int runningsufix = 1;
-        for(int i = leng -1; i >= 0; i--){
-            ret[i] *= runningsufix;
-            runningsufix *= nums[i];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        
+        int rp = 1;
+        
+        left[0] = 1;
+        right[right.length-1] = 1;
+        
+        for(int i = 1; i < left.length; i++){
+            rp *= nums[i-1];
+            left[i] = rp;
         }
-        return ret;
-
+        
+        rp = 1;
+        
+        for(int i = right.length-2; i >= 0; i--){
+            rp *= nums[i+1];
+            right[i] = rp;
+        }
+        
+        
+        
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = left[i] * right[i];
+        }
+        
+        // System.out.println(Arrays.toString(nums));
+        return nums;
     }
 }
