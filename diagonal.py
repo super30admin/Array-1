c100d34c689553e11026c3fee84b499568ab8d59
yahska-ednihs <75634279+yahska-ednihs@users.noreply.github.com>
2021-01-14 19:38:45 -0800

    Submitting Array 1

---
+++ diagonal.py
@@ -0,0 +1,7 @@
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : No
+# Three line explanation of solution in plain english:
+# I am not able to solve this yet.
+
+# Your code here along with comments explaining your approach
