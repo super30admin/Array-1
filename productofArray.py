5518041a64b15c259b3a532b25e5e9b6759bb614
sunigupt <sunil.1.gupta@nokia.com>
2021-05-04 16:15:47 -0700

    Array-2

---
+++ productofArray.py
@@ -0,0 +1,30 @@
+from typing import  List
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[]}
+    def productExceptSelf(self, nums):
+        p = 1
+        n = len(nums)
+        output = []
+        for i in range(0,n):
+            output.append(p)
+            p = p * nums[i]
+        p = 1
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * p
+            p = p * nums[i]
+        return output
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print(Solution().productArray(arrylist=[1,2,3,4]))
