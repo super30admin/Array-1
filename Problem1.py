859e0675c28929d9804b3b5751f818e625d03e84
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-06-07 13:45:10 +0530

    Completed Problem1

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,30 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+# Calculating product first and keeping track of zeros if zeros cross one then everything will be zero else everything will be zero
+# except for the valuje zero element
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        zero = 0
+        prod = 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                prod *= nums[i]
+            else:
+                zero += 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if zero == 0:
+                    nums[i] = prod // nums[i]
+                else:
+                    nums[i] = 0
+            else:
+                if zero == 1:
+                    nums[i] = prod
+                else:
+                    nums[i] = 0
+        return nums
+            
+        
