0d32e1e6a2037be2fcb5175d25f24a1182d7da21
Akarsh-Chandrashekar <49974714+Akarsh-Chandrashekar@users.noreply.github.com>
2021-06-08 16:47:03 -0700

    added comments

--- problem1.cpp
+++ problem1.cpp
@@ -1,3 +1,11 @@
+// Problem 1
+// Time Complexity :O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes 
+// Three line explanation of solution in plain english
+// finding right and left sum of the element 
+// use 2 loops to compute left and right sum  and used the result array to maintain O(1) space complexity 
+
 #include<iostream>
 #include<vector>
 #include<string>
