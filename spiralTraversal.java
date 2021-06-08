c3abd19c08dcd84be5a92611393abbd39a1647b2
Kishan Sinojia <kishansinojia@Kishans-MacBook.local>
2021-06-08 17:52:12 -0400

    Completed Array-1

---
+++ spiralTraversal.java
@@ -0,0 +1,44 @@
+//Time Complexity:  O(m x n) where m is number of rows and n is number of columns in given input array
+//Space Complexity: O(1)
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        List<Integer> result = new ArrayList<>();
+        
+        int m = matrix.length; int n = matrix[0].length;
+        int top = 0; int bottom = m-1;
+        int left = 0; int right = n-1;
+        
+        while(left <= right && top <= bottom){
+            
+            for(int j=left; j <= right; j++){
+                result.add(matrix[top][j]);
+            }
+            top++;
+        
+            for(int i=top; i <= bottom; i++){
+                result.add(matrix[i][right]);
+            }
+            right--;
+            
+            if(top <= bottom){
+                for(int j=right; j >= left; j--){
+                    result.add(matrix[bottom][j]);
+                }
+            }
+            
+            bottom--;
+
+            if(left <= right){
+                for(int i=bottom; i >= top; i--){
+                    result.add(matrix[i][left]);
+                }
+            }
+            left++;
+            
+        }
+        
+        return result;
+    }
+}
