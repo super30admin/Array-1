9383a141417a9ae6bc9552ad30e3901c45301630
Akarsh-Chandrashekar <49974714+Akarsh-Chandrashekar@users.noreply.github.com>
2021-06-08 17:00:51 -0700

    added comments

--- problem3(spiral).cpp
+++ problem3(spiral).cpp
@@ -1,3 +1,13 @@
+// Problem 2=3
+// Time Complexity :O(m*n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes 
+// Three line explanation of solution in plain english
+// maintain four pointers to print the spirals start from the largest spiral and gp inward  update the pointers appropriately after completion of each segment of spiral
+// do this continously intill the pointers cross over since the base case variable is being chnged check if base case holds true inside the loop for cases where it is not implicitly checked by for loop
+// return result of traversal in intger vector 
+
+
 #include<iostream>
 #include<vector>
 #include<string>
