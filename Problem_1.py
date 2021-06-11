47b1dfd4e66dbc5ee8ada20f39aa2f08fb16d97a
AdithyaHolla <56132230+AdithyaHolla@users.noreply.github.com>
2021-06-11 12:40:57 -0700

    Create Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,25 @@
+"""
+Time Complexity : O(n)
+Space Complexity : O(1)
+    
+"""
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if not nums or len(nums) == 0:
+            return []
+        
+        mul = 1 
+        rlist = [1] * len(nums)
+        rlist[0] = 1 
+        
+        for i in range(1,len(nums)):
+            mul *= nums[i-1]
+            rlist[i] = mul  
+        
+        mul = 1 
+        for i in range(len(nums)-2, -1, -1):
+            mul = mul * nums[i+1]
+            rlist[i] = rlist[i] * mul
+        
+        return rlist 
