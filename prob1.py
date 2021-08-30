0692e468619a417ba84085b7c1526e0bd3bfbaf4
Smriti0311 <smritigrao@gmail.com>
2021-08-30 14:47:01 -0400

    Product except self with space O(1)

--- prob1.py
+++ prob1.py
@@ -1,23 +1,26 @@
 # Time Complexity : O(N)
-# Space Complexity : O(N) for left and right arrays
+# Space Complexity : O(1) as no extra space
 # Did this code successfully run on Leetcode : yes
 # Three line explanation of solution in plain english : find the left product, 
 # right product and the result is the product of left and right 
 def prodself(nums):
     
     n = len(nums)
-    left = [0] * n
-    right = [0] * n
-    left[0] = 1
-    right[n-1] = 1
-    
+    res = [1] * n
+    lp = 1
+
     for i in range(1, n):
-        left[i] = left[i-1] * nums[i-1]
+        lp = lp * nums[i-1]
+        res[i] = lp
     
-    for i in range(n-2, -1, -1):
-        right[i] = right[i+1] * nums[i+1]
+    rp = 1
+    for j in range(n-2, -1, -1):
+        rp = rp * nums[j+1]
+        res[j] *= rp
     
-    return [left[i] * right[i] for i in range(n)]
+    return res
+    
+    
 
 nums = [1,2, 3, 4]
 print(prodself(nums))
