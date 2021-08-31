68546ee9bc4dc6ca6cd9db2bf7d8040767cc5e79
Abhirag Nagpure <nagpureabhirag@gmail.com>
2021-08-31 17:23:46 -0500

    Completed Sprint Array-1: Please Review

--- problem_2.py
+++ problem_2.py
@@ -1,22 +1,38 @@
-# Did this code successfully run on Leetcode : NO
+# Did this code successfully run on Leetcode : YES
+
+# TC: O(m*n)
+# SC: O(1)
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        visited = set()
-        m = len(matrix)
-        n = len(matrix[0])
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m = len(mat)
+        n = len(mat[0])
         result = []
+        dir = 1
+        c = 0
         i, j = 0, 0
-        while len(visited) <= m*n:
-            result.append(matrix[i][j])
-            visited.add((i, j))
-            print(matrix[i][j])
-            if i == 0 or (i-1, j) in visited:
-                j += 1
-            elif j == n-1 or (i, j+1) in visited:
-                i += 1
-            elif i == m-1 or (i+1, j) in visited:
-                j -= 1
-            elif j == 0 or (i, j-1) in visited:
-                i -= 1
+        while c < m*n:
+            # print((mat[i][j]))
+            result.append(mat[i][j])
+            c += 1
+            if dir == 1:
+                if j == n-1:
+                    dir = -1
+                    i += 1
+                elif i == 0:
+                    dir = -1
+                    j += 1
+                else:
+                    j += 1
+                    i -= 1
+            else:
+                if i == m-1:
+                    dir = 1
+                    j += 1
+                elif j == 0:
+                    dir = 1
+                    i += 1
+                else:
+                    i += 1
+                    j -= 1
         return result
