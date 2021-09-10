1bfad49d03914ea4a48b145062bca59226ea0340
ShanmathiS <59035995+ShanmathiS@users.noreply.github.com>
2021-09-09 17:34:19 -0700

    Update DiagonalTraversal.cpp

--- DiagonalTraversal.cpp
+++ DiagonalTraversal.cpp
@@ -1,5 +1,5 @@
 // Time Complexity :O(N)
-// Space Complexity : O(N)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
 
