61ae16ab5b1700e91dbafeb0ba0bf9bb4c251a78
Adhish Shrivastava <adhish70@gmail.com>
2021-10-19 16:57:10 -0400

    Problem1 done

--- Problem-1.py
+++ Problem-1.py
@@ -6,4 +6,17 @@
 
 class Solution:
     def productExceptSelf(self, nums):
-        pass
+        left = [1]*len(nums)
+        
+        for i in range(1,len(nums)):
+            left[i] = left[i-1]*nums[i-1]
+        
+        right = 1
+        for i in range(len(nums)-1,-1,-1):
+            left[i] = left[i]*right
+            right *= nums[i]
+            
+        return left
+
+obj = Solution()
+print(obj.productExceptSelf([1,2,3,4]))
