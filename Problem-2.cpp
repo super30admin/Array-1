e2889a7248313fb33ecb86e22355e56b60807574
Durgesh Patil <patildurgesh1998@gmail.com>
2021-10-26 17:34:15 -0400

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -1,7 +1,24 @@
 //Time Complexity = O(M*N)
 //Space Complexity = O(1)
 //Here the Time complexity is O(1) because the extra space (vector<int> ans) which we are using is the same which we are returning.
+/*Question: Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order as shown in the below image.
 
+Example:
+
+Input:
+
+[
+
+[ 1, 2, 3 ],
+
+[ 4, 5, 6 ],
+
+[ 7, 8, 9 ]
+
+]
+
+Output: [1,2,4,7,5,3,6,8,9]
+*/
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
