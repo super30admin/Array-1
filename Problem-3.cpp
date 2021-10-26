61b3fa2b25ea8b23390af704a99fc7778f97fb95
Durgesh Patil <patildurgesh1998@gmail.com>
2021-10-26 17:34:52 -0400

    Update Problem-3.cpp

--- Problem-3.cpp
+++ Problem-3.cpp
@@ -2,6 +2,34 @@
 //Space Complexity = O(1)
 //Where M is the number of rows and N is the number of coloumns in the matrix.
 //The space complexity is O(1) because the extra space (vector<int> ans) is the same which we our function is returning.
+/*Question: Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input:
+
+[
+
+[ 1, 2, 3 ],
+
+[ 4, 5, 6 ],
+
+[ 7, 8, 9 ]
+
+] Output: [1,2,3,6,9,8,7,4,5] Example 2:
+
+Input:
+
+[
+
+[1, 2, 3, 4],
+
+[5, 6, 7, 8],
+
+[9,10,11,12]
+
+] Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+*/
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
