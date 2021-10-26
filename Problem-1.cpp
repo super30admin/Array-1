4e16be1c80d2ab68a7577ac1980b027cc93d0cb5
Durgesh Patil <patildurgesh1998@gmail.com>
2021-10-26 17:33:40 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -1,6 +1,13 @@
 //Time Complexity = O(N)
 //Space Complexity = O(N)
 //Here the space complexity is O(N) because the function return type is vector and we are taking the extra space(vector<int> temp1,vector<int> temp2) but returning the vector<int> temp1.
+/*Question: Given an array nums of n integers where n > 1, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input: [1,2,3,4] Output: [24,12,8,6] Note: Please solve it without division and in O(n).
+
+Follow up: Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)*/
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
