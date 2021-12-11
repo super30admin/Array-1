8bf4a03d9e9b67683fed305d202fafab8d770141
Taran <ktaran14@gmail.com>
2021-12-11 21:46:25 +0530

    Arrays-1 done.

--- ProductArrayExceptItself.java
+++ ProductArrayExceptItself.java
@@ -1,25 +1,33 @@
-//TC - O(n)
-//Space - O(1)
-// leetcode passed - Yes
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// For finding product at any ith index, calculate the right product on the fly using a variable
+// calculate the left product in an array
+// Use leftArray and rightProduct to obtain the result
+
+// Your code here along with comments explaining your approach
 public class ProductArrayExceptItself {
     public int[] productExceptSelf(int[] nums) {
 
         int n = nums.length;
-        int [] left = new int[n];
+        int [] left = new int[n]; // this will keep left array product first, eventually will use this as resultant array
 
-        int rp =1;
+        int rp =1;  // variable to keep the right elements product
 
         left[0] = 1;
+        // Find the left elements product
         for(int i =1; i<n;i++) {
             left[i] = left[i-1] * nums[i-1];
         }
 
-
+        // Store product of right and left in ith element
+        // Update the right product
         for(int i = n-1; i>=0; i--) {
             left[i] = rp*left[i];
             rp = rp*nums[i];
         }
 
+        // contains our result
         return left;
     }
 }
--- SpiralTraversal.java
+++ SpiralTraversal.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//TC - O(m*n)
-// Space - O(1)
-// Leetcode passed - Yes
+// Time Complexity : O(mn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
 
 public class SpiralTraversal {
 
@@ -15,32 +16,42 @@ public class SpiralTraversal {
         int m = matrix.length;
         int n = matrix[0].length;
 
-
-        int top =0, bottom = m-1;
-        int left = 0, right = n-1;
+        // Taking four variables to keep track of the four direction traversal
+        int top =0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n-1;
         List<Integer> ans = new ArrayList<> ();
 
+        // Add check so that the pointers donot cross each other and we end up adding duplicate elements
         while(top <=bottom && left <=right) {
+            // Traverse left to right
             for(int i = left ; i<=right; i++) {
                 ans.add(matrix[top][i]);
             }
-            top++;
+            top++; // as we have traversed the row at top
+
+            // Traverse from top to bottom - because we want to make a spiral
             for(int i = top; i<=bottom; i++) {
                 ans.add(matrix[i][right]);
             }
-            right--;
-            if(top <=bottom) {
+            right--; // move towards left direction, because we traversed the right column elements in above for loop
+
+            if(top <=bottom) {  // Since we changed the top in line 32, we need to check it back again if it didn't break the condition
+                // traverse right to left
                 for(int i = right; i>=left; i--) {
                     ans.add(matrix[bottom][i]);
                 }
             }
-            bottom--;
-            if(left<=right) {
+            bottom--; // move towards top direction, because we traversed the bottom row elements in above for loop
+
+            if(left<=right) {  // Since we changed the right in line 38, we need to check it back again if it didn't break the condition
+                // traverse from bottom to top
                 for(int i = bottom; i>=top; i--) {
                     ans.add(matrix[i][left]);
                 }
             }
-            left++;
+            left++; // move toward the right direction
         }
         return ans;
     }
