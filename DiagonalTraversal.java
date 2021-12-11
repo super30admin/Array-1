ef16fdc3619a6a2b2a1b5dd1053181a9ff7ebca1
Taranjit Kaur <taranjit@chegg.com>
2021-12-11 21:46:25 +0530

    Arrays-1 done.

--- DiagonalTraversal.java
+++ DiagonalTraversal.java
@@ -1,7 +1,14 @@
+// Time Complexity : O(mn)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+// Approach
+// We take dir variable to determine the direction in which we want to move at any point
+// we observe the general trend of i, j for both downward and upward direction
+// we observe the corner cases to handle and where the dir needs to be reversed
+
 public class DiagonalTraversal {
 
-    //TC - O(mn)
-    // Space - O(1)
     public int[] findDiagonalOrder(int[][] mat) {
 
         if(mat ==null || mat.length ==0) {
@@ -10,32 +17,44 @@ public class DiagonalTraversal {
 
         int m = mat.length;
         int n = mat[0].length;
+
+        // Creating 1D matrix to return the ans array
         int [] ans = new int[m*n];
 
-        int dir =1;
+        int dir =1; // variable to determine the direction
         int i=0;
         int j=0;
         for(int k=0; k<m*n;k++) {
             ans[k] = mat[i][j];
             if(dir ==1) {
+                // corner case - when we have reached the last column, reverse the dir and increment row
                 if(j==n-1) {
                     i++;
                     dir= -1;
-                } else if(i==0) {
+                }
+                // corner case - when we are at first row, increment the column, reverse the direction
+                else if(i==0) {
                     j++;
                     dir=-1;
-                } else {
+                }
+                // general case - decrement row, increment column
+                else {
                     i--;
                     j++;
                 }
             } else {
+                // corner case - when we have reached the last row, reverse the dir and increment column
                 if(i==m-1) {
                     j++;
                     dir=1;
-                } else if(j==0) {
+                }
+                // corner case - when we are at first column, increment the row, reverse the direction
+                else if(j==0) {
                     i++;
                     dir=1;
-                } else {
+                }
+                // general case - increment row, decrement column
+                else {
                     i++;
                     j--;
                 }
