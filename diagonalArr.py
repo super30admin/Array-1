dde38263956d3013057f31370220b7a463035d6f
Aniket Hirpara <gdg4dev@gmail.com>
2022-01-05 22:37:39 +0530

    fixed diagonal arr porblem on leetcode

--- diagonalArr.py
+++ diagonalArr.py
@@ -1,21 +1,23 @@
 # TC: O(N)
 # SC: O(1)
 #LC : YESs
+# difficulty: i pre-assumed that the both mat and mat[0] length is going to be same
+# always, spent 2 days figuring out this one problem why leetcode wasn't working. got myself super confused.
+#finally understood what mistake i made
 from typing import List
 
 
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        if not mat or not len(mat) or not len(mat[0]) or len(mat) != len(mat[0]):
+        if not mat or not len(mat) or not len(mat[0]):
             return [-1]
-        result = [None for i in range(len(mat)*len(mat))]
-        curr = 0
+        result = []
         row, col, d = 0, 0, 1
-        while(curr< len(mat) * len(mat)):
-            result[curr] = mat[row][col]
+        while(curr< len(mat) * len(mat[0])):
+            result.append(mat[row][col])
             curr+=1
             if(d== 1):
-                if col == len(mat) -1:
+                if col == len(mat[0]) -1:
                     row+=1
                     d=-1
                 elif row == 0:
@@ -36,4 +38,4 @@ class Solution:
                     row +=1
         return result
 sol = Solution()
-print(sol.findDiagonalOrder2([[1,2,3],[4,5,6],[7,8,9]]))
+print(sol.findDiagonalOrder([[2,3]]))
