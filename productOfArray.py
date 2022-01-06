82ce308e01b7974a685ffcbf0f95437c8b865a91
Aniket Hirpara <gdg4dev@gmail.com>
2022-01-06 18:45:15 +0530

    python solution

--- productOfArray.py
+++ productOfArray.py
@@ -1,4 +1,7 @@
-# some erros remains. I'll fix it ASAP
+# TC = O(N)
+# SC = O(1)
+# LEETCODE = YES
+# PROBLEMS = NO
 
 from typing import List
 
@@ -6,18 +9,28 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         size = len(nums)
-        result = [None for i in range(size)]
-        result[0] = 1
-        runningProduct = 1
-        for i in range(len(nums)+1,0):
-            runningProduct *= nums(i-1)
-            result[i] = runningProduct
+        result = []
+        runningProduct = None
+          
+        for i in range(size):
+            if i == 0:
+                runningProduct = nums[i]
+            else:
+                runningProduct *= nums[i]
+            result.append(runningProduct) 
+        runningProduct = None
         
-        for i in range(size-2,0,-1):
-            runningProduct *= nums[i+1]
-            result[i] = result[i] * runningProduct
+        for i in range(size-1,-1,-1):
+            if i == size - 1:
+                result[i] = result[i-1]
+                runningProduct = nums[i]
+            elif i == 0:
+                result[i] = runningProduct
+            else:
+                result[i] = result[i-1] * runningProduct
+                runningProduct = nums[i] * runningProduct
+                
         return result
-            
         # TC = NxN
         # loop through all the element and get their indexes
         # for i in range(len(nums)):
@@ -32,7 +45,7 @@ class Solution:
             
         #     result.append(product)
             
-        return result
+        # return result
 
 
 
