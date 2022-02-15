770480e253a7493d4e8534a224aa38d00499c2b7
NiharikaMorishetti <nmorishetti@hawk.iit.edu>
2022-02-14 23:47:21 -0500

    please review spiralmatrix

--- Sample.java
+++ Sample.java
@@ -4,3 +4,39 @@
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> a = new ArrayList<Integer>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top =0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+        int matSize = m*n;
+        while(a.size() < matSize){
+            //forward
+            for(int i=left;i<=right && a.size()<matSize;i++){
+                a.add(matrix[top][i]);
+            }
+            top++;
+            //downward
+            for(int i=top;i<=bottom && a.size()<matSize;i++){
+                a.add(matrix[i][right]);
+            }
+            right--;
+            //backward
+            for(int i=right;i>=left && a.size()<matSize;i--){
+                a.add(matrix[bottom][i]);
+            }
+            bottom--;
+            //upward
+            for(int i=bottom;i>=top && a.size()<matSize;i--){
+                a.add(matrix[i][left]);
+            }
+            left++;
+            
+        }
+        return a;
+    }
+}
