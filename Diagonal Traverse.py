2d056bead58169b9b86c55ca9582044e85a224ff
Mani9333 <manikanta.ellenki9@gmail.com>
2022-05-15 03:26:52 -0700

    Added Recursive Code for Spiral

--- Diagonal Traverse.py
+++ Diagonal Traverse.py
@@ -41,48 +41,6 @@ class Solution:
 
 
 
-# Recursive Solution
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if matrix == [] or len(matrix) == 0:
-            return []
-        
-        
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        l,r,t,b,ind = 0,n-1,0,m-1,0
-        
-        li = list()
-        
-        self.helper(l,r,t,b,li,matrix)
-        return li
-    
-    def helper(self,l,r,t,b,li,matrix):
-        
-        if l > r and t > b:
-            return
-        
-        if l <= r and t <= b:
-            for i in range(l,r+1):
-                li.append(matrix[t][i])
-            t += 1
-            
-        if l <= r:
-            for i in range(t,b+ 1):
-                li.append(matrix[i][r])    
-            r -= 1
 
-        if t <= b:
-            for i in range(r,l-1,-1):
-                li.append(matrix[b][i])
-            b -= 1
-
-        if l <= r:
-            for i in range(b,t-1,-1):
-                li.append(matrix[i][l])
-            l += 1
-        
-        self.helper(l,r,t,b,li,matrix)
         
         
