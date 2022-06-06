540022b4f303901bdc3f2add5744ee560994d04b
ishankaggarwal <ishank193aggarwal@gmail.com>
2022-06-07 00:29:07 +0530

    "Completed Product of Array Except Self"

---
+++ Product of Array Except Self.cpp
@@ -0,0 +1,23 @@
+//Time Complexity- O(n)
+//Space Complexity- O(n)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        vector<int> output(nums.size(),0);
+        
+        output[0]=1;
+        for(int i=1;i<nums.size();i++){
+            output[i]=output[i-1]*nums[i-1];
+        }
+        
+        int r=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            output[i]=r*output[i];
+            r=r*nums[i];
+        }
+        
+        return output;
+    }
+};
