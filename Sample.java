ee69be0c01611aafde93a24d7b99b3b62fbdee4f
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-07 14:10:29 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -103,3 +103,51 @@ class Solution {
       return res;  
     }
 }
+//****SPIRAL MATRIX****
+// Time Complexity : O(M*N);
+// Space Complexity :O(1);
+// Did this code successfully run on Leetcode : y
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int top=0;
+        int left=0;
+        int right=n-1;
+        int bottom=m-1;
+        List<Integer> res= new ArrayList<>();
+        
+        while(left<=right && top<=bottom)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            
+                for(int i=top;i<=bottom;i++)
+                {
+                    res.add(matrix[i][right]);
+                }
+            right--;
+            if(top<=bottom)
+            {
+                for(int i=right;i>=left;i--)
+                {
+                    res.add(matrix[bottom][i]);
+                }
+            }
+            bottom--;
+            if(left<=right)
+            {
+                for(int i=bottom;i>=top;i--)
+                {
+                    res.add(matrix[i][left]);
+                }
+            }
+            left++;
+        }
+        
+        return res;
+    }
+}
