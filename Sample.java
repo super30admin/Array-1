d0ab6267839d2eca2b273ee390ea98108151bcb8
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-07 13:21:54 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -32,3 +32,74 @@ class Solution {
         return result;
     }
 }
+//****DIAGONAL MATRIX****
+// Time Complexity : O(M*N);
+// Space Complexity :O(1);
+// Did this code successfully run on Leetcode : y
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m= mat.length;
+        int n=mat[0].length;
+        
+        //null case:
+        if(mat.length==0)
+        {
+            return new int[0];
+        }
+        
+        int[] res=new int[m*n];
+        int i=0;
+        int j=0;
+        int idx=0;
+        int dir=1;
+        
+        while(idx<m*n)
+        {
+            res[idx]=mat[i][j];
+            idx++;
+            
+            //upward
+            if(dir==1)
+            {
+                if(i==0 && j!=n-1)
+                {
+                    j++;
+                    dir=-1;
+                    
+                }
+                else if(j==n-1)
+                {
+                    i++;
+                    dir=-1;
+                }
+                else
+                {
+                    i--;
+                    j++;
+                }
+            }
+            //downward
+            else
+            {
+                if(j==0 && i!=m-1)
+                {
+                    i++;
+                    dir=1;
+                    
+                }
+                else if(i==m-1)
+                {
+                    j++;
+                    dir=1;  
+                }
+                else
+                {
+                    j--;
+                    i++;
+                }
+                
+            }
+        }
+      return res;  
+    }
+}
