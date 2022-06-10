7f6ddd3dc5b6a7d80c46e79a9a3b4c3a60db782a
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-09 18:23:17 -0700

    added time complexity

--- findDiagonalOrder.py
+++ findDiagonalOrder.py
@@ -1,3 +1,7 @@
+# Time Complexity : O(m*n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         num_rows=len(mat)
