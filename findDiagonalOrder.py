1fe82d0ab555ca6176c5a015803d0955dc9cff45
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-09 18:22:15 -0700

    q2

---
+++ findDiagonalOrder.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        num_rows=len(mat)
+        num_cols=len(mat[0])        
+    #looked up how to get no of diagnols from matrix formula is (rows +coulms )-1
+        d=[[] for _ in range((num_rows+num_cols)-1)]
+        for i in range(num_rows):
+            for j in range(num_cols):
+                d[i+j].append(mat[i][j])
+        
+        output=d[0]  
+
+        for x in range(1, len(d)):
+            if x%2==1:
+                output.extend(d[x])
+            else:
+                output.extend(d[x][::-1])
+        return output
