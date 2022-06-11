1b23f64172ec18ebe61e3ddcd5e8bdf15f8a7c33
Dixika Grewal <59345244+dixg@users.noreply.github.com>
2022-06-11 03:03:36 -0700

    q2

--- findDiagonalOrder.py
+++ findDiagonalOrder.py
@@ -20,3 +20,35 @@ class Solution:
             else:
                 output.extend(d[x][::-1])
         return output
+   
+# class Solution:
+#     def findDiagonalOrder(self, mat: list[list[int]]) -> list[int]:
+#         i = 0
+#         j = 0
+#         rows = len(mat)
+#         cols = len(mat[0])
+#         up = True
+#         result = []
+#         while i < rows and j < cols:
+#             result.append(mat[i][j])
+#             if up:
+#                 if j == cols - 1:
+#                     i += 1
+#                     up = False
+#                 elif i == 0:
+#                     j += 1
+#                     up = False
+#                 else:
+#                     i -= 1
+#                     j += 1
+#             else:
+#                 if i == rows - 1:
+#                     j += 1
+#                     up = True
+#                 elif j == 0:
+#                     i += 1
+#                     up = True
+#                 else:
+#                     i += 1
+#                     j -= 1
+#         return result
