433cef6ca9574857b6e4cf05421372d89251c740
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-01 20:31:36 -0400

    Update SpiralArray.java

--- SpiralArray.java
+++ SpiralArray.java
@@ -1,3 +1,10 @@
+// Time Complexity : O(m*n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// maintain four boundries of the matrix as top,bottom,left,right. print the outer sprial using for loops. then update the value of top,bottom,left,right 
+// accordingly and execute the same loops with new boundaries. if at any point we are changing varible on which while loop is dependent, check those 
+// conditions again before executing further.
+
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
        int top = 0,bottom = matrix.length-1;
