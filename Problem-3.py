047045d32587015b0bc5e8012727b26fc80f5644
Saiteja Garlapati <86909787+lostrider07@users.noreply.github.com>
2022-08-03 15:37:13 -0700

    Completed Problem 3

---
+++ Problem-3.py
@@ -0,0 +1,31 @@
+# Time complexity : O(m*n)
+# Space complexity: O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        result = []
+        if len(matrix) == 0:
+            return res
+        row_begin = 0
+        col_begin = 0
+        row_end = len(matrix)-1 
+        col_end = len(matrix[0])-1
+        while (row_begin <= row_end and col_begin <= col_end):
+            for i in range(col_begin, col_end + 1):
+                result.append(matrix[row_begin][i])
+            row_begin += 1
+            for i in range(row_begin, row_end + 1):
+                result.append(matrix[i][col_end])
+            col_end -= 1
+            if (row_begin <= row_end):
+                for i in range(col_end, col_begin-1, -1):
+                    result.append(matrix[row_end][i])
+                row_end -= 1
+            if (col_begin <= col_end):
+                for i in range(row_end, row_begin-1, -1):
+                    result.append(matrix[i][col_begin])
+                col_begin += 1
+        return result
+    
