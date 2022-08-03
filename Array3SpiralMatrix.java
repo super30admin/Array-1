7a95d28554cf55a6db8823467f27b6fe76e2d733
NehaGidwani <109174051+NehaGidwani@users.noreply.github.com>
2022-08-03 22:54:48 +0530

    Add files via upload

---
+++ Array3SpiralMatrix.java
@@ -0,0 +1,13 @@
+// Time Complexity : O(m*n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class Solution {
+	public int[] SpiralArray(int[][] nums){
+		
+	}
+}
