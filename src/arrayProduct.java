8cf5d239afe842fee8c6893f8fb860e77cc3d11e
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-08-29 21:35:05 -0400

    product of array except self

---
+++ src/arrayProduct.java
@@ -0,0 +1,25 @@
+public class arrayProduct{
+    public int[] productExceptSelf(int[] nums) {
+        
+        int[] arr = new int[nums.length];
+        int runProd = 1;
+        arr[0] = 1;
+    
+        for(int i=1; i< nums.length; i++){
+            
+            runProd *= nums[i-1];
+            arr[i] = runProd;
+            
+        }
+        
+        runProd = 1;
+        
+        for(int i= nums.length-2; i>=0; i--){
+            runProd *= nums[i+1];
+            arr[i] *= runProd; 
+        }
+        
+        return arr;
+        
+    }
+}
