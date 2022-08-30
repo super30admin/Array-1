df60dbceecb342d7d90b682ad0dd91e17301dcaa
Saurabh Bodke <saurabhbodke012@gmail.com>
2022-08-29 21:38:34 -0400

    diagonal traversal

---
+++ src/DiagonalTraversalMatrix.java
@@ -0,0 +1,58 @@
+public class DiagonalTraversalMatrix {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int n= mat.length;
+        int m= mat[0].length;
+        
+        int[] result = new int[n*m];
+        
+        if(mat.length == 0 || mat[0].length == 0){
+            return new int[0];
+        }
+        
+        int i=0;
+        int row =0;
+        int col =0;
+        
+        boolean flag = true;
+        
+        while( row<n && col<m){
+            
+            if( flag ){
+                
+                while( row>0 && col < m-1){
+                    result[i++] = mat[row][col];
+                    row--;
+                    col++;
+                }
+                result[i++] = mat[row][col];
+                
+                if(col == m-1){
+                    row++;
+                }else{
+                    col++;
+                }
+                
+            }else{
+                
+                while( col>0 && row < n-1){
+                    result[i++] = mat[row][col];
+                    row++;
+                    col--;
+                }
+                result[i++] = mat[row][col];
+                
+                if(row == n-1){
+                    col++;
+                }else{
+                    row++;
+                }
+                
+            }
+            
+            flag = !flag;
+        }
+        
+        return result;
+        
+    }
+}
