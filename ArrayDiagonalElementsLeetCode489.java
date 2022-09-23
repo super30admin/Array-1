9a325d5ff84ac22b0d554e02c7e6d16e00100564
khushbukareliya07 <khushbu.karelia@gmail.com>
2022-09-23 12:36:47 -0500

    Leetcode 54 spiral matrix, leetcode  Diagonal matrix is added.

--- ArrayDiagonalElementsLeetCode489.java
+++ ArrayDiagonalElementsLeetCode489.java
@@ -1,78 +1,69 @@
-package Array_1.DiagonalElements;
-import java.util.Arrays;
-//Leetcode: 498
-public class DiagonalArrayElements
-{
-    public int[] findDiagonalOrder(int[][] mat) {
-        int row = 0, col =0, m = mat.length, n = mat[0].length;
-        int len = 0, dir =1;
-        int[] result = new int[m*n];
-        System.out.println(m*n);
 
-        while(len < m*n)
+/*
+* approach: consider the edge cases while going up, and change the direction to down
+*up case edges == row ==0, col == n-1
+*down case = edge case - col ==0 and row ==m-1!!
+* same way, for down direction, consider the edge cases and change the direction accordingly.
+*
+* Time Complexity: O(M*N) m=row, n = col
+* Space Complexity: O(1)
+* Whatever space we are using for storing, we are returning the same array, so that will not count towards space/ */
+
+
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        if(mat == null || mat.length == 0) return new int[]{};
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        int idx = 0;
+        int dir =1; //up, down == -1;
+        int row =0, col =0;
+        
+        int[] result = new int[m*n];
+        
+        while(idx < m*n)
         {
-            System.out.println("length : "+len + "  row= "+row+ "  col= "+col );
-            result[len] = mat[row][col];
-            if(dir ==1)
+            result[idx++] = mat[row][col];
+            
+            
+            if(dir ==1) //UP direction
             {
                 if(col == n-1)
                 {
-                    row++;
-                    dir =-1;
-                }
-                else if(row == 0)
-                {
-                    col++;
                     dir = -1;
+                    row++;
+                }
+                else if(row ==0)
+                {
+                    dir = -1;
+                    col++;
                 }
                 else
                 {
                     row--;
                     col++;
                 }
-
             }
-            else //dir = -1
+            else//DOWN direction
             {
-                if(row == n-1)
+                if(row == m-1)
                 {
-                    col++;
                     dir =1;
+                    col++;
                 }
                 else if(col == 0)
                 {
-                    row++;
                     dir = 1;
+                    row++;
                 }
                 else
                 {
-                    row++;
                     col--;
+                    row++;
                 }
             }
-            len++;
         }
         return result;
-
     }
 }
-
-class Main
-{
-    public static void main(String[] args) {
-        int[][] mat = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-
-        DiagonalArrayElements d = new DiagonalArrayElements();
-        int[] arr = d.findDiagonalOrder(mat);
-        System.out.println(Arrays.toString(arr));
-    }
-}
-
-
-/*
-* approach: consider the edge cases while going up, and change the direction to down
-* same way, for down direction, consider the edge cases and change the direction accordingly.
-*
-* Time Complexity: O(N)
-* Space Complexity: O(1)
-* Whatever space we are using for storing, we are returning the same array, so that will not count towards space/ */
--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,62 +1,47 @@
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SpiralMatrix {
-    public List<Integer> spiralOrder(int[][] matrix)
-    {
-        List<Integer> result = new ArrayList<>();
+/*
+* Approach: considered Edge Case: move right, down, left, up
+* rotated through array in the same manner from left to right, and vice versa; top to bottom and vice versa.
+* top =0 top row, left =0 left col, right = n-1 last col, bottom = m-1 last row
+* Space Complexity: O(1) - No auxiliary space used.
+* Time Complexity: O(M*N)
+*  */
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        if(matrix == null || matrix.length ==0) return new ArrayList<Integer>();
+        
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int top =0, bottom = m-1, left =0, right = n-1, len=0;
-        while (top <= bottom && left <=right)
+        int top =0, bottom =m-1, left =0, right =n-1; 
+        List<Integer> result = new ArrayList<>();
+        
+        while(top <= bottom && left <= right)
         {
-            //going left to right
-            for(int i = left; i<= right; i++)
+            //left to right, col changes
+            for(int  j= left; j<= right; j++)
             {
-                result.add(matrix[top][i]);
+                result.add(matrix[top][j]);
             }top++;
-
-            //top to bottom
-            for(int i =top; i <= bottom ; i++)
+            
+            //top to bottom at right col, row changes
+            for(int i =top; i<= bottom; i++)
             {
                 result.add(matrix[i][right]);
             }right--;
-
-            //going right to left
-            for(int i = right; i >=left && top<=bottom; i-- )
+            
+            //right to left, col changes
+            for(int j = right; j>= left && top <= bottom; j--)
             {
-                result.add(matrix[bottom][i]);
+                result.add(matrix[bottom][j]);
             }bottom--;
-
-            //going up
-            for(int i =bottom; i >= top && left<=right; i--)
+            
+            //bottom to top, row changes
+            for(int i= bottom; i>= top && left<= right; i--)
             {
                 result.add(matrix[i][left]);
             }left++;
         }
-
-        return  result;
+    return result;
+        
     }
 }
-
-class Main
-{
-    public static void main(String[] args) {
-        int[][] matrix = new int[][]{{1,2,3,4},{10,11,12,5},{9,8,7,6}};
-
-        SpiralMatrix s = new SpiralMatrix();
-        List<Integer> list = s.spiralOrder(matrix);
-        System.out.println(list);
-    }
-}
-
-
-/*
-* Approach: considered Edge Case: move right, down, left, up
-* rotated through array in the same manner from left to right, and vice versa; top to bottom and vice versa.
-*
-* Space Complexity: O(1) - No auxiliary space used.
-* Time Complexity: O(M*N)
-*  */
