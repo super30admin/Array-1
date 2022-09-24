07770dbaf552c5264e9305970b4080dbdaf47948
Samrita Sen Bakshi <samrita.bakshe@gmail.com>
2022-09-23 17:08:34 -0700

    Problem 2 & 3 completed

---
+++ Problem2.js
@@ -0,0 +1,40 @@
+//Time: O(n*m) n= rows, m=columns
+//Space: O(n*m)
+//Solutions runs on leetcode and is a accpted solution.
+ var findDiagonalOrder = function(mat) {
+    let r = 0;
+    let c = 0;
+    let n = mat[0].length;
+    let m = mat.length;
+    let dir=1,idx=0;
+    let result = Array(m*n).fill(0);
+    while(idx < m * n){
+        result[idx] = mat[r][c];
+        idx++;
+        if(dir == 1){
+            if(c == n-1 ){
+               dir =-1;
+                r++;
+            }else if(r==0){
+                dir =-1;
+                c++;    
+            }else{
+               c++;
+               r--;
+            }
+        }else{
+             if( r == m-1){
+                dir = 1;
+                c++;
+            }else if(c==0){
+                dir = 1;
+                r++;          
+            }else{
+               r++;
+               c--;
+            }
+        }
+    }
+    return result
+    
+};
---
+++ Problem3.js
@@ -0,0 +1,35 @@
+//Time: O(n*m), Space: O(n*m)
+//Solution is accepted and runs on leetcode
+
+ var spiralOrder = function(matrix) {
+    if(matrix.length == 0 ) return [];
+    let rows = matrix.length;
+    let columns = matrix[0].length;
+    let left= 0, right= columns-1, top= 0, bottom = rows-1;
+    let result = [];
+    while(top<=bottom && left <= right){
+        for(let i=left; i<=right; i++){
+            result.push(matrix[top][i])
+        }
+        top++;
+        if(top<=bottom && left <= right){
+            for(let i=top; i<=bottom; i++){
+                result.push(matrix[i][right])
+            }
+            right--;
+        }
+        if(top<=bottom && left <= right){
+            for(let i=right; i>=left; i--){
+                result.push(matrix[bottom][i])
+            }
+            bottom--;
+        }
+        if(top<=bottom && left <= right){
+             for(let i=bottom; i>=top; i--){
+                result.push(matrix[i][left])
+            }
+            left++;
+        }
+    }
+    return result;
+};
