dcd5340640c199d163570e3cf97b7959d6d67d42
Sumanth Shankar <sumanth@rezy360.com>
2022-09-27 13:02:46 -0700

    Solution for product of array except self

--- ProductArraySolution.java
+++ ProductArraySolution.java
@@ -1,10 +1,28 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Three line explanation of solution in plain english
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
 
-// Your code here along with comments explaining your approach
+// Going throught the array in 2 passes.
+// In the 1st pass adding prefix product.
+// In the 2nd pass from reverse adding the postfix product.
 
 public class ProductArraySolution {
-    
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        int prefix = 1;
+        
+        for(int i = 0; i < nums.length; i += 1) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        
+        int postfix = 1;
+        for(int i = nums.length - 1; i >= 0; i -= 1) {
+            result[i] *= postfix;
+            postfix *= nums[i];
+        }
+        
+        return result;
+    }
 }
