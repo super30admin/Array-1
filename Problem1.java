ade4505629984c4f43e815094a911baa3581fe7f
Prerana Rajeshvar Kharat <preranarajeshvarkharat@Preranas-MacBook-Pro.local>
2022-09-30 16:22:28 -0700

    Completed Array-1

---
+++ Problem1.java
@@ -0,0 +1,27 @@
+//T.C=O(n)
+//S.C= O(1)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+
+        if(nums == null || nums.length == 0)
+            return left;
+
+        int rp = 1;
+        left[0] = 1;
+
+        //create the left array
+        for(int i=1; i<nums.length; i++){
+            rp = rp * nums[i-1];
+            left[i] = rp;
+        }
+
+        //reset rp = 1 and compute the right side
+        rp = 1;
+        for(int i=nums.length-2; i>=0; i--){
+            rp = rp * nums[i+1];
+            left[i] = left[i] * rp;
+        }
+        return left;
+    }
+}
