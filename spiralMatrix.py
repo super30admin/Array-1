2de6bed3e4d470b4ef136d5da0886d2c38b11744
Vignesh Viswanathan <vigneshviswanathan@Vigneshs-MBP.lan>
2022-10-26 22:55:54 -0500

    Completed Spiral Matrix

---
+++ spiralMatrix.py
@@ -0,0 +1,42 @@
+# Space Complexity: O(1)
+# Time Complexity: O(mn) : m -> Rows, n -> columns
+
+# Approach is to translate the traversal into different for loops and check the edge condition.
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        result = []
+        
+        if matrix is None or m == 0:
+            return result
+        
+        
+        top = 0
+        bottom = m - 1
+        left = 0
+        right = n - 1
+        
+        while((left <= right) and (top <= bottom)):
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+            
+            if (left <= right):
+                for i in range(top, bottom + 1):
+                    result.append(matrix[i][right])
+                right -= 1
+                
+            if (top <= bottom):
+                for i in reversed(range(left, right + 1)):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            
+            if (left <= right):
+            
+                for i in reversed(range(top, bottom + 1)):
+                    result.append(matrix[i][left])
+                left += 1
+        
+        return result
