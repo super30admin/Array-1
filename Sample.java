2469e9a8b7deee41e522d75381006edbb459972f
MalavyaRaval <malavyaraval@gmail.com>
2023-03-18 20:38:14 -0700

    Completed

--- Sample.java
+++ Sample.java
@@ -1,6 +1,27 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
-
+// we created an empty integer list where we can store the multiplication of rest of the didgits in the input list
 // Your code here along with comments explaining your approach
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if(nums == null || nums.length == 0) return new int[0];
+        int [] result = new int[nums.length];
+        int rp = 1;
+        int temp = 1;
+        for(int i=0;i<nums.length;i++){
+            rp = rp*temp;
+            result[i] = rp;
+            temp = nums[i];
+        }
+        rp = 1;
+        temp = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            rp = rp*temp;
+            result[i] = result[i] * rp;
+            temp = nums[i];
+        }
+    return result;
+    }
+}
