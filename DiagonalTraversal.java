001166395990bd67f77c468936ed460ba6d854aa
kulkarnisneha <sneha.kulkarni@verizonconnect.com>
2023-06-05 20:31:39 -0400

    problem2

--- DiagonalTraversal.java
+++ DiagonalTraversal.java
@@ -1,76 +1,59 @@
 // Time Complexity : O(m*n)
-// Space Complexity : O(m*n)
-// Did this code successfully run on Leetcode :yes
+// Space Complexity :O(1) as the output array will not be taken into consideration here
+// Did this code successfully run on Leetcode :
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
 class DiagonalTraversal {
-    boolean goingUp; //up
-    int rows;
-    int cols;
     public int[] findDiagonalOrder(int[][] mat) {
 
         if(mat == null || mat.length <1){
             return null;
         }
+        int rows = mat.length;;
+        int cols  = mat[0].length;
+        boolean goingUp = true; //up
 
-        rows = mat.length;
-        cols = mat[0].length;
         int[] result = new int[rows*cols];
         int i=0, j =0;
-        goingUp = true;
 
-        result[0] = mat[0][0];
-        for(int k=1; k<result.length; k++){
-            boolean toggle = false;
+
+        for(int k=0; k<result.length;){
             if(goingUp){
-                boolean topEnd=false, rightEnd=false;
-                if ( i - 1 < 0 ) topEnd=true;
-                if ( j + 1 >= cols ) rightEnd=true;
-                toggle = true;
+                do {
+                    result[k++] = mat[i--][j++];
+                } while ( ( i >= 0  &&  j < cols) ) ;
 
-                if ( ! topEnd && !rightEnd ) {
-                    i = i- 1;
-                    j = j + 1;
-                    toggle=false;
-                } else if ( topEnd && rightEnd ) {
-                    i = 1;
-                    j = cols - 1;
-                } else if ( topEnd ) {
-                    i = 0;
-                    j = j + 1;
+
+                if ( i < 0 ) {
+                    if ( j >= cols ) {
+                        i = 1;
+                        j = cols - 1;
+                    } else {
+                        i = 0;
+                    }
                 } else {
-                    i = i + 1;
+                    i = i + 2;
                     j = cols - 1;
                 }
-
             }else{
-                boolean bottomEnd=false, leftEnd=false;
-                if ( i + 1 >= rows ) bottomEnd=true;
-                if ( j - 1 < 0  ) leftEnd=true;
-                toggle = true;
+                do {
+                    result[k++] = mat[i++][j--];
+                } while ( ( i < rows  &&  j >= 0 ) );
 
-                if ( !bottomEnd && !leftEnd ) {
-                    i = i+1;
-                    j = j-1;
-                    toggle=false;
-                } else if ( bottomEnd && leftEnd ) {
-                    i = rows - 1;
-                    j = 1;
-                } else if ( bottomEnd ) {
-                    i = rows - 1;
-                    j = j + 1;
+                if ( ( i >= rows )  ) {
+                    if (j < 0 ){
+                        i = rows - 1;
+                        j = 1;
+                    } else {
+                        i = rows - 1;
+                        j = j + 2;
+                    }
                 } else {
-                    i = i + 1;
                     j = 0;
                 }
-
             }
-            if(toggle){
-                goingUp =!goingUp;
-            }
-
-            result[k] = mat[i][j];
+            goingUp =!goingUp;
         }
         return result;
     }
