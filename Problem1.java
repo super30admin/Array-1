77ff6de95faffd40ca527ab425f9315a0e7994ab
PurnaSahithiAdduri <sahithiadduri06@gmail.com>
2023-07-14 07:30:35 -0700

    Optimized solution

--- Problem1.java
+++ Problem1.java
@@ -1,5 +1,5 @@
 // Time Complexity :O(n)
-// Space Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
 // Three line explanation of solution in plain english
 
@@ -9,20 +9,18 @@
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] L = new int[nums.length];
-        int[] R = new int[nums.length];
-        int[] ans = new int[nums.length];
-         L[0] = 1;
-        R[nums.length-1] = 1;
+        int[] result = new int[nums.length];
+        int rp = 1;
+        result[0]=1;
         for(int i=1;i<nums.length;i++){
-            L[i] = nums[i-1]*L[i-1];
+            rp = rp * nums[i-1];
+            result[i] = rp;
         }
-        for(int i=nums.length-2;i>=0;i--){
-            R[i] = nums[i+1]*R[i+1];
+        rp = 1;
+        for(int i=nums.length-2;i>=0;i--){      
+            rp = rp*nums[i+1];
+            result[i] *= rp; 
         }
-        for(int i=0;i<nums.length;i++){
-            ans[i] = L[i]*R[i];
-        }
-        return ans;
+        return result;
     }
 }
