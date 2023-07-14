0b1d9b825556e0e051e366f88130118b842314a8
PurnaSahithiAdduri <sahithiadduri06@gmail.com>
2023-07-14 07:31:35 -0700

    Optimized solution

--- Problem2.java
+++ Problem2.java
@@ -1,5 +1,5 @@
 // Time Complexity :O(m*n)
-// Space Complexity : O(n)
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode : yes
 // Three line explanation of solution in plain english
 
