08a7f7785af0adf1d32006b562a684edab98d045
God7wyn <jamesgodwynj@gmail.com>
2024-01-02 01:23:51 -0500

    Updated Time Complexity

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,5 +1,5 @@
 /*
- * Time Complexity : O(n*m)
+ * Time Complexity : O(n)
  * Space Complexity : O(1)
  * Did this code successfully run on Leetcode : Yes
  * Any problem you faced while coding this : No
