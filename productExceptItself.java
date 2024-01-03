c752d5a775de8dd9b64ba7ab7f2efe853f6d9004
Albert Massey <eralbert9191@gmail.com>
2024-01-02 19:11:12 -0800

    product expept itself

--- productExceptItself.java
+++ productExceptItself.java
@@ -1,28 +1,27 @@
-//time complexity O(2n) => O(n)
-//space complexity O(1)
-//APPROACH: find the left running product and store them in the result array
-//then find the right running product and update the result array by multiplying current values with right running product. This will ensure left and right running products are multiplied but not the number itself
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Three line explanation of solution in plain english: Calculate running product for each element while doing the left pass and store in corresponding index in the result[]. Then do the same for right pass but multiply already existing value in result[] with current running product and update the value.
+
+// Your code here along with comments explaining your approach
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if(nums == null || nums.length == 0) return new int[0];
+        if (nums.length < 2) return new int[0];
         int[] result = new int[nums.length];
         //left pass
-        int lTemp = 1;
-        int lRp = 1;
-        for(int i = 0; i < nums.length; i++){
-            lRp = lRp * lTemp;
-            result[i] = lRp;
-            lTemp = nums[i];
-        }
-        //right pass
-        int rTemp = 1;
         int rp = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            rp = rp * rTemp;
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            rp *= nums[i-1];
+            result[i] = rp;
+        } 
+        rp = 1;
+        for (int i = nums.length - 2; i >= 0; i--){
+            rp *= nums[i+1];
             result[i] *= rp;
-            rTemp = nums[i];
         }
         return result;
     }
 }
+
