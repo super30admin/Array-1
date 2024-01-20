c3d7ad35e99835481727423f36265ff1db4fe066
Tanmay Singal <tanmaysingal@icloud.com>
2024-01-19 17:47:49 -0800

    238. Product of Array Except Self.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,29 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
 // Three line explanation of solution in plain english
+// Calculate prod of right of each index and then multiply by running product of left of each index on second iteration
 
 // Your code here along with comments explaining your approach
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        result[result.length-1] = 1;
+
+        for(int i = result.length-2; i > -1; i--){
+            result[i] = result[i+1] * nums[i+1];
+        }
+
+        int runningProduct = 1;
+
+        for(int i = 0; i < result.length; i++){
+            result[i] = result[i] * runningProduct;
+            runningProduct *= nums[i];
+        }
+
+        return result;
+        
+    }
+}
